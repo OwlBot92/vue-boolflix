5ee99d4b6b8eec24b407676a0e3c51246724b2fc
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2021-04-30 14:06:18 +0200

    aggiunto cast alle schede

--- js/script.js
+++ js/script.js
@@ -5,11 +5,16 @@ var app = new Vue({
         // API RELATED
         searchedFilms : [],
         searchedSeries: [],
+
+        currentCast : [],
+
         search: "",
         urlFilm: "https://api.themoviedb.org/3/search/movie?api_key=e05661b069389f9a2788162b272f96a8&language=it-IT&query=",
         urlSerie: "https://api.themoviedb.org/3/search/tv?api_key=e05661b069389f9a2788162b272f96a8&language=it-IT&query=",
+
         imgStartPath: "https://image.tmdb.org/t/p/w342",
 
+        
         //STYLING RELATED
         widthGrow : "",
     },
@@ -17,7 +22,7 @@ var app = new Vue({
 
     methods: {
 
-        /* ----- FUNZIONI LEGATE AL FUNZIONAMENTO E ALLA MANIPOLAZIONE DELLE API ----- */
+        /* ----- FUNZIONI LEGATE ALLA MANIPOLAZIONE DELLE API ----- */
         searchBy(){
             if (this.search.length) {
                 //CHIAMATA per i film con titolo che corrispondono al campo di ricerca
@@ -25,22 +30,55 @@ var app = new Vue({
                     .get(`${this.urlFilm}${this.search}`)
                     .then((response) => {
                         let ris1 = response.data.results;
-                        
+                        let castFilmsArray = [];
                         this.searchedFilms = this.filterByLang(ris1);
                         this.voteTransform(this.searchedFilms);
+                        for (const obj of this.searchedFilms) {
+                            this.getCast(obj.id, "movie")
+                                .then(data => {
+                                    obj.credits = data
+                                    castFilmsArray.push(obj);
+                                    this.searchedFilms = castFilmsArray;
+                                })
+                        }
                     })
+
                 //CHIAMATA per le serie con titolo che corrisponde al campo di ricerca
                 axios
                     .get(`${this.urlSerie}${this.search}`)
                     .then((response) => {
                         let ris2 = response.data.results;
-                        
+                        let castSerieArray = []
                         this.searchedSeries = this.filterByLang(ris2);
                         this.voteTransform(this.searchedSeries);
+                        for (const obj of this.searchedSeries) {
+                            this.getCast(obj.id, "tv")
+                                .then(data => {
+                                    obj.credits = data
+                                    castSerieArray.push(obj);
+                                    this.searchedSeries = castSerieArray;
+                                })
+                        }
                     })
             }
             this.search = "";
         },
+        //per estrapolare il cast
+        getCast(id, from){
+            let url = `https://api.themoviedb.org/3/${from}/${id}/credits?api_key=e05661b069389f9a2788162b272f96a8`
+            return axios
+                .get(url)
+                .then((response) => {
+                    let cast = [];
+                    let castArray = response.data.cast;
+                    castArray = castArray.slice(0, 5);
+                    for (const obj of castArray) {
+                        cast.push(obj.name);
+                    }
+                    return cast;
+                })
+        },
+
         //trasforma il voto
         voteTransform(array){
             return array.forEach((obj) => obj.vote_average = Math.ceil(obj.vote_average / 2));
@@ -49,7 +87,6 @@ var app = new Vue({
         filterByLang(array){
             return array.filter((obj) => obj.original_language == "en" && obj.poster_path != null)
         },
-
         /* ----- FUNZIONI PER LO STYLING ----- */
         searchExpand(){
             (this.widthGrow.length) ? this.widthGrow = "" : this.widthGrow = "width-grow";
@@ -59,3 +96,27 @@ var app = new Vue({
         
     }
 })
+
+//PER IL CAST
+//https://api.themoviedb.org/3/movie/181812/credits?api_key=e05661b069389f9a2788162b272f96a8&query=
+
+
+
+/* 
+let url = `https://api.themoviedb.org/3/movie/${film.id}/credits?api_key=e05661b069389f9a2788162b272f96a8`
+axios
+    .get(url)
+    .then((response) => {
+        let castArray = response.data.cast;
+        let cast = [];
+        castArray = castArray.slice(0, 5);
+        console.log(castArray)
+        for (const obj of castArray) {
+            cast.push(obj.name);
+        }
+        film.credits = cast;
+    }) */
+
+
+
+
