0e887e79d1a276f24960cc0e8287940be842b0ca
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2021-05-01 16:09:46 +0200

    finita la ms6

--- js/script.js
+++ js/script.js
@@ -2,43 +2,90 @@ var app = new Vue({
     el: "#root",
     data: {
 
-        // API RELATED
+        /* ----- API RELATED ----- */
         searchedFilms : [],
         searchedSeries: [],
 
-        currentCast : [],
+        //Insieme di generi delle serie Tv
+        serieGenreSet : new Set(),
+    
+        //Insieme di generi dei FILM
+        filmGenreSet : new Set(),
+        
 
+        //filtro select SERIE TV
+        selectChoiceSerie : "ALL",
+        //filtro select FILM
+        selectChoiceFilm: "ALL",
+
+        //field di ricerca film/serie tv
         search: "",
         urlFilm: "https://api.themoviedb.org/3/search/movie?api_key=e05661b069389f9a2788162b272f96a8&language=it-IT&query=",
         urlSerie: "https://api.themoviedb.org/3/search/tv?api_key=e05661b069389f9a2788162b272f96a8&language=it-IT&query=",
-
+        //prima parte dell'url per le immagini
         imgStartPath: "https://image.tmdb.org/t/p/w342",
 
         
-        //STYLING RELATED
+        /* ----- STYLING RELATED ---- */
         widthGrow : "",
     },
     
 
-    methods: {
+    /*
+    MORE
+    - implementare possibilità di cercare premendo invio
+    - implementare ritorno alla pagina iniziale cliccando su BOOLFLIX
+    - commentare nel dettaglio il funzionamento della funzione searchBy()
+    - rimuovere console.log di debug
 
+    MS4
+    - implementare lingua => bandiera
+     
+    */
+
+    methods: {
         /* ----- FUNZIONI LEGATE ALLA MANIPOLAZIONE DELLE API ----- */
         searchBy(){
+            //resetta il contenuto dell insieme dei generi delle serie tv ogni volta che si cerca qualcosa di nuovo
+            this.serieGenreSet.clear();
+            this.filmGenreSet.clear();
+            // per evitare di cercare un genere che non c'è tra i film/ serie quando si effettua una nuova ricerca
+            this.selectChoiceSerie = "ALL";
+            this.selectChoiceFilm = "ALL";
+
             if (this.search.length) {
-                //CHIAMATA per i film con titolo che corrispondono al campo di ricerca
+                //CHIAMATA per i film con titolo che corrisponde al campo di ricerca
                 axios
                     .get(`${this.urlFilm}${this.search}`)
                     .then((response) => {
                         let ris1 = response.data.results;
                         let castFilmsArray = [];
+                        let genreMovieArray = [];
                         this.searchedFilms = this.filterByLang(ris1);
                         this.voteTransform(this.searchedFilms);
                         for (const obj of this.searchedFilms) {
+                            //CAST
                             this.getCast(obj.id, "movie")
                                 .then(data => {
                                     obj.credits = data
                                     castFilmsArray.push(obj);
                                     this.searchedFilms = castFilmsArray;
+                                })
+                            //GENERI da aggiungere, come nelle serie
+                            this.getGenres(obj.id, "movie")
+                                .then(data => {
+                                    if (!data.length) {
+                                        obj.generi = ["N/A"];
+                                    }
+                                    else {
+                                        let temp = [];
+                                        for (const iterator of data) {
+                                            temp.push(iterator.name);
+                                        }
+                                        obj.generi = temp;
+                                    }
+                                    genreMovieArray.push(obj)
+                                    this.searchedFilms = genreMovieArray;
                                 })
                         }
                     })
@@ -48,21 +95,65 @@ var app = new Vue({
                     .get(`${this.urlSerie}${this.search}`)
                     .then((response) => {
                         let ris2 = response.data.results;
-                        let castSerieArray = []
+                        let castSerieArray = [];                           
+                        let genreSerieArray = [];
                         this.searchedSeries = this.filterByLang(ris2);
                         this.voteTransform(this.searchedSeries);
                         for (const obj of this.searchedSeries) {
+                            //CAST
                             this.getCast(obj.id, "tv")
                                 .then(data => {
-                                    obj.credits = data
+                                    (data.length) ? obj.credits = data : obj.credits = ["N/A"]
                                     castSerieArray.push(obj);
                                     this.searchedSeries = castSerieArray;
                                 })
+                            //GENERI
+                            this.getGenres(obj.id, "tv")
+                                .then(data => {
+                                    if (!data.length) {
+                                        obj.generi = ["N/A"];
+                                    }
+                                    else{
+                                        let temp = [];
+                                        for (const iterator of data) {
+                                            temp.push(iterator.name);
+                                        }
+                                        obj.generi = temp;
+                                    }
+                                    genreSerieArray.push(obj)
+                                    this.searchedSeries = genreSerieArray;
+                                })
                         }
+                        console.log(this.searchedSeries);
+                        
                     })
             }
             this.search = "";
         },
+
+        //per estrapolare i generi
+        getGenres(id, from){
+            let url = `https://api.themoviedb.org/3/${from}/${id}?api_key=e05661b069389f9a2788162b272f96a8`
+            return axios
+                .get(url)
+                .then((response) => {
+                    //(from == "movie") ? this.filmGenreSet.add("ALL") : this.serieGenreSet.add("ALL");
+                    if (from == "movie") {
+                        this.filmGenreSet.add("ALL");
+                        for (const obj of response.data.genres) {
+                            this.filmGenreSet.add(obj.name);
+                        }
+                    }
+                    else{
+                        this.serieGenreSet.add("ALL");
+                        for (const obj of response.data.genres) {
+                            this.serieGenreSet.add(obj.name);
+                        }
+                    }
+                    return response.data.genres
+                })
+        },
+
         //per estrapolare il cast
         getCast(id, from){
             let url = `https://api.themoviedb.org/3/${from}/${id}/credits?api_key=e05661b069389f9a2788162b272f96a8`
@@ -78,7 +169,21 @@ var app = new Vue({
                     return cast;
                 })
         },
-
+        //filtra in base al valore selezionato
+        filterBySelect(objArray, valore) {
+            let filteredArray = [];
+            if (valore == "ALL") {
+                filteredArray = objArray;
+            }
+            else{
+                for (const film of objArray) {
+                    if (film.generi.includes(valore)) {
+                        filteredArray.push(film);
+                    }
+                }
+            }
+            return filteredArray;
+        },
         //trasforma il voto
         voteTransform(array){
             return array.forEach((obj) => obj.vote_average = Math.ceil(obj.vote_average / 2));
@@ -92,30 +197,10 @@ var app = new Vue({
             (this.widthGrow.length) ? this.widthGrow = "" : this.widthGrow = "width-grow";
         }
     },
+    //inutilizzata
     mounted(){
-        
     }
 })
-
-//PER IL CAST
-//https://api.themoviedb.org/3/movie/181812/credits?api_key=e05661b069389f9a2788162b272f96a8&query=
-
-
-
-/* 
-let url = `https://api.themoviedb.org/3/movie/${film.id}/credits?api_key=e05661b069389f9a2788162b272f96a8`
-axios
-    .get(url)
-    .then((response) => {
-        let castArray = response.data.cast;
-        let cast = [];
-        castArray = castArray.slice(0, 5);
-        console.log(castArray)
-        for (const obj of castArray) {
-            cast.push(obj.name);
-        }
-        film.credits = cast;
-    }) */
 
 
 
